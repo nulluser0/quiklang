96f60a036bf47ae46205185ebf0748382d754404
nulluser0 <diuyueng@gmail.com>
2024-07-18 18:43:38 +1000

    adksjf;lakdsfj

--- src/backend_vm/compiler.rs
+++ src/backend_vm/compiler.rs
@@ -1,6 +1,6 @@
 // Compiler
 
-use crate::{backend_vm::bytecode::BCMetadata, errors::VMCompileError, frontend::ast::Stmt};
+use crate::{backend_vm::bytecode::BCMetadata, errors::VMCompileError, frontend::ast::{Expr, Stmt}};
 
 use super::bytecode::{BCIntegrityInfo, ByteCode};
 
@@ -50,7 +50,7 @@ impl Compiler {
 
     fn compile_statement(bytecode: &mut ByteCode, stmt: Stmt) -> Result<(), VMCompileError> {
         match stmt {
-            Stmt::ExprStmt(_) => todo!(),
+            Stmt::ExprStmt(expr) => Self::compile_expression(bytecode, expr)?,
             Stmt::DeclareStmt {
                 name,
                 is_mutable,
@@ -75,5 +75,28 @@ impl Compiler {
             Stmt::AliasDefStmt { ident, alias } => todo!(),
         }
         Ok(())
+    }
+
+    fn compile_expression(bytecode: &mut ByteCode, expr: Expr) -> Result<(), VMCompileError> {
+        match expr {
+            Expr::Literal(_) => todo!(),
+            Expr::Array(_, _) => todo!(),
+            Expr::Identifier(_) => todo!(),
+            Expr::Tuple(_) => todo!(),
+            Expr::AssignmentExpr { assignee, expr } => todo!(),
+            Expr::ConcatOp { left, right } => todo!(),
+            Expr::BinaryOp { op, left, right } => todo!(),
+            Expr::UnaryOp(_, _) => todo!(),
+            Expr::FunctionCall(_, _) => todo!(),
+            Expr::Member(_, _) => todo!(),
+            Expr::IfExpr { condition, then, else_stmt } => todo!(),
+            Expr::ForExpr { identifier, iterable, then } => todo!(),
+            Expr::WhileExpr { condition, then } => todo!(),
+            Expr::BlockExpr(_) => todo!(),
+            Expr::ForeverLoopExpr(_) => todo!(),
+            Expr::SpecialNull => todo!(),
+            Expr::StructLiteral(_, _) => todo!(),
+            Expr::EnumLiteral(_, _, _) => todo!(),
+        }
     }
 }
