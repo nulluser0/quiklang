cd7a0f8690e0d2944ced7631dd0b9840eab17c75
nulluser0 <diuyueng@gmail.com>
2024-10-29 18:00:11 +1100

    seperation between 'array' (fixed) and 'list array' (resizable)

--- src/lib/common/src/data_structs/ast/expr/array_expr.rs
+++ src/lib/common/src/data_structs/ast/expr/array_expr.rs
@@ -12,16 +12,29 @@ use crate::errors::Span;
 use super::Expr;
 
 /// Array expression.
-/// Represents an array.
-/// Example: `[1, 2, 3, 4, 5]`.
-/// Arrays are dynamically sized collections of values.
-/// The compiler however can infer if the array is a fixed-size array.
+/// Represents a fixed size array.
+/// Example: `[1, 2, 3, 4, 5]`. '[T; N]'.
+/// Arrays are fixed sized collections of values.
+/// For dynamic sized collections, use a list.
 #[derive(Debug, Clone)]
 pub struct ArrayExpr {
     /// The values in the array.
     pub values: Vec<Expr>,
     /// The size of the array.
     pub size: Option<usize>,
+    /// Span
+    pub span: Span,
+}
+
+/// List Array Expression.
+/// Represents a dynamic sized array.
+/// Example: `list[1, 2, 3, 4, 5]`. 'list[T]'
+/// Lists are dynamic sized collections of values.
+/// For fixed sized collections, use an array.
+#[derive(Debug, Clone)]
+pub struct ListArrayExpr {
+    /// The values in the list.
+    pub values: Vec<Expr>,
     /// Span
     pub span: Span,
 }
