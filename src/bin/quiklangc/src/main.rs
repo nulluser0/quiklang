09fb70de9dac54f21574d519ee094d44374be2be
null <151502370+nulluser0@users.noreply.github.com>
2024-11-12 11:22:25 +1100

    Add semantic checker and integrate quiklang-common dependency; update main.rs with compilation pathway documentation

--- src/bin/quiklangc/src/main.rs
+++ src/bin/quiklangc/src/main.rs
@@ -1,3 +1,11 @@
+//! The compilation pathway is as follows:
+//! 1. Files are gathered into a file store. Package.toml is read.
+//! 2. Dependency tree is built from the file store, with dependencies being resolved.
+//! 3. For each dependency starting from the top of the tree:
+//! 4. Beginning at root module (main or lib), the parser reads the file and generates an AST.
+//! 5. The AST is passed to the semantic checker, which verifies that the AST is semantically correct.
+
 fn main() {
     println!("Hello, world!");
 }
+
--- src/lib/frontend/semantic-checker/src/lib.rs
+++ src/lib/frontend/semantic-checker/src/lib.rs
@@ -1,1 +1,45 @@
+//! # Semantic Checker
+//!
+//! The semantic checker is responsible for verifying that the AST is semantically correct.
+//!
 
+use quiklang_common::{data_structs::ast::package_module::{Module, Package}, CompilationReport};
+
+pub struct SemanticAnalyzer<'a> {
+    compilation_report: &'a mut CompilationReport,
+    package: &'a mut Package,
+}
+
+impl<'a> SemanticAnalyzer<'a> {
+    pub fn new(compilation_report: &'a mut CompilationReport, package: &'a mut Package) -> Self {
+        Self {
+            compilation_report,
+            package,
+        }
+    }
+
+    /// Analyze the AST to ensure that it is semantically correct.
+    pub fn analyze(&mut self) {
+        // Analyze library if it exists.
+        if let Some(lib) = &self.package.library {
+            self.analyze_module(lib);
+        }
+
+        // Analyze each bin module.
+        for bin in &self.package.binaries {
+            self.analyze_module(bin.module);
+        }
+    }
+
+    /// Analyze a module.
+    fn analyze_module(&mut self, module: &Module) {
+        
+        // Resolve names in the AST.
+        self.resolve_names();
+    }
+
+    /// Resolve names in the AST.
+    fn resolve_names(&mut self) {
+        
+    }
+}
